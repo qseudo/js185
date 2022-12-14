5733e5cadc63e84388421ceae640f825149fcc83
qseudo <57642143+qseudo@users.noreply.github.com>
2022-12-14 11:57:04 +0900

    fix

---
+++ todos/lib/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password)
+  VALUES ('admin', '$2b$10$TvavWmRrIJJk/IAdo3XdvOsxYxGIYg1xfLfr2JkLVaH2SR9A/dmya'),
+         ('developer', '$2b$10$D.jqfUrIPPPm46wnkNq4cOJ2D8.IMctFWp7rIyODVreckcjq8ULdG'),
+         ('somebody', 'knock-knock');
